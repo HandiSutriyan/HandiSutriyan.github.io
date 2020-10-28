35157385b7d4fdea94de35e437700a87a37781f8
HandiSutriyan <handisutrian@gmail.com>
2020-10-28 10:34:37 +0700

    cannot run push.js

---
+++ js/notif.js
@@ -0,0 +1,69 @@
+const requestPermisson = () => {
+	Notification.requestPermission().then((result)=>{
+		if (result === "denied") {
+          console.log("Fitur notifikasi tidak diijinkan.");
+          return;
+        } else if (result === "default") {
+          console.error("Pengguna menutup kotak dialog permintaan ijin.");
+          return;
+        }
+
+        console.log("Fitur notifikasi diijinkan.");
+	})
+}
+
+// Push notifikasi lokal
+const pushNotif = (message) => {
+	const title = "Meneer Bola";
+	const options = {
+		'body': `Pemberitahuan \n ${message}`,
+		'icon': '/assets/icon.png',
+		'badge': '/assets/icon.png',
+	}
+
+	if (Notification.permission == "granted") {
+		navigator.serviceWorker.ready.then((registration) => {
+			registration.showNotification(title, options);
+		});
+	}else {
+		console.error('FItur notifikasi tidak diijinkan.');
+	}
+}
+
+//deskripsi public key
+const urlBase64ToUint8Array = (base64String) => {
+	const padding = '='.repeat((4 - base64String.length % 4) % 4);
+	const base64 = (base64String + padding)
+		.replace(/-/g, '+')
+		.replace(/_/g, '/');
+	const rawData = window.atob(base64);
+	const outputArray = new Uint8Array(rawData.length);
+	for (let i = 0; i < rawData.length; ++i){
+		outputArray[i] = rawData.charCodeAt(i);
+	}
+	return outputArray;
+}
+
+if ("Notification" in window){
+	requestPermisson();
+}else{
+	console.error("Browser tidak mendukung notifikasi.");
+}
+
+//subscribe web push
+if (('PushManager' in window)) {
+	navigator.serviceWorker.getRegistration().then((registration)=>{
+		registration.pushManager.subscribe({
+			userVisibleOnly: true,
+			applicationServerKey: urlBase64ToUint8Array("BBFZLjvizFoF6qX79KTXyQNwr2F30f684clKHQ5yit4tj6QMrGdcl4x-nExLZ7KsdS9jM4sYR_dG_MkFiZmRlKA")
+		}).then((subscribe)=>{
+			console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+			console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+				null, new Uint8Array(subscribe.getKey('p256dh')))));
+			console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                null, new Uint8Array(subscribe.getKey('auth')))));
+		}).catch((e)=>{
+			console.error('Tidak bisa subscribe', e.message);
+		});
+	});
+}
--- js/push.js
+++ js/push.js
@@ -1,36 +1,32 @@
-const requestPermisson = () => {
-	Notification.requestPermission().then((result)=>{
-		if (result === "denied") {
-          console.log("Fitur notifikasi tidak diijinkan.");
-          return;
-        } else if (result === "default") {
-          console.error("Pengguna menutup kotak dialog permintaan ijin.");
-          return;
-        }
+const webPush = require('web-push');
 
-        console.log("Fitur notifikasi diijinkan.");
-	})
-}
+const vapidKeys = {
+   "publicKey": "BBFZLjvizFoF6qX79KTXyQNwr2F30f684clKHQ5yit4tj6QMrGdcl4x-nExLZ7KsdS9jM4sYR_dG_MkFiZmRlKA",
+   "privateKey": "_89QD3v6iuPmH26g4hFCXBFZWX1jlS67G-XGk1wD1DM"
+};
 
-const pushNotif = (message) => {
-	const title = "Meneer Bola";
-	const options = {
-		'body': `Pemberitahuan \n ${message}`,
-		'icon': '/assets/icon.png',
-		'badge': '/assets/icon.png',
-	}
+webPush.setVapidDetails(
+   'mailto:example@yourdomain.org',
+   vapidKeys.publicKey,
+   vapidKeys.privateKey
+);
 
-	if (Notification.permission == "granted") {
-		navigator.serviceWorker.ready.then((registration) => {
-			registration.showNotification(title, options);
-		});
-	}else {
-		console.error('FItur notifikasi tidak diijinkan.');
-	}
-}
+var pushSubscription = {
+   "endpoint": "https://fcm.googleapis.com/fcm/send/dB4xNQybMFA:APA91bFU-kysZ9rDo860Wm9gGVppjkWQS4K35lRj8OQPTjkH0kK_0kdEgkEnLW59zoBJWWOF0BIJRhQCzIn5AVIo25xY8N4Ff0KBvyRMTWBlfxoMkAVIAfyiKMWxvTCbMY3q1507zfiw",
+   "keys": {
+       "p256dh": "BJgAOopcYbDOCWFtEZeCbspmKYThdSDs93MqHTeIZTYmSGsTnIyKNlYYQVeodw1bHuPIdFv/uxFpJkxUWkflppk=",
+       "auth": "mSEF40RvIZ+nStfVlQWXTw=="
+   }
+};
 
-if ("Notification" in window){
-	requestPermisson();
-}else{
-	console.error("Browser tidak mendukung notifikasi.");
-}
+var payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
+
+var options = {
+   gcmAPIKey: '196273722426',
+   TTL: 60
+};
+webPush.sendNotification(
+   pushSubscription,
+   payload,
+   options
+);
